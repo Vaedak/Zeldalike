875f35fa74c2b669d77083b364168e13e322748b
Vaedak <67328279+Vaedak@users.noreply.github.com>
2022-04-09 23:47:55 +0200

    Add files via upload

---
+++ Project_Zeldalike_0.15/zeldalike_sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="zeldalike_sprite" tilewidth="16" tileheight="16" tilecount="703" columns="37">
+ <image source="Textures/zeldalike_sprite.png" width="600" height="312"/>
+</tileset>
