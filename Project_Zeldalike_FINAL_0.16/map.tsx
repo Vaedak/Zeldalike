528ea3cdfc31d7faaffc02f5adea717d817a0b64
Vaedak <67328279+Vaedak@users.noreply.github.com>
2022-04-10 15:26:15 +0200

    Add files via upload

---
+++ Project_Zeldalike_FINAL_0.16/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="map" tilewidth="16" tileheight="16" tilecount="10000" columns="100">
+ <image source="Textures/map.png" width="1600" height="1600"/>
+</tileset>
